04382961b44074625a0fda7f9edc3f3a49633ef1
ablos <dieterich.abel@gmail.com>
2025-09-21 10:34:07 +0200

    Secrets example

---
+++ include/secrets_example.h
@@ -0,0 +1,13 @@
+// =======================================================================
+//
+// Rename this file to secrets.h and replace the values with your secrets.
+//
+// =======================================================================
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "your-ssid"
+#define WIFI_PASS "your-password"
+
+#endif
